468050f90004dcec4541626dde43d31b8d81618a
JPPinkyLabs <jp@pinkylabs.io>
2026-03-17 19:39:28 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,16 +106,16 @@ export default function Home() {
         </span>
         <div className="max-w-[1200px] mx-auto relative grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-20 items-center">
           <SectionReveal>
-            <div className="relative max-w-[400px] md:max-w-none">
-              <Image
-              src="/images/valpo-cocktail-week-flyer.png"
-              alt="Valpo Cocktail Week 2026"
-              fill
-              className="object-cover"
-              />
-              <div className="absolute bottom-[-16px] right-[-16px] w-[60%] h-[60%] border-r-2 border-b-2 border-magenta shadow-[8px_8px_30px_rgba(255,45,120,0.2)]" />
-              <div className="absolute top-[-16px] left-[-16px] w-[40%] h-[40%] border-l-2 border-t-2 border-calipso shadow-[-8px_-8px_30px_rgba(0,229,204,0.2)] z-[1]" />
-            </div>
+<div className="relative max-w-[400px] md:max-w-none aspect-[4/5] overflow-hidden">
+  <Image
+    src="/images/valpo-cocktail-week-flyer.png"
+    alt="Valpo Cocktail Week 2026"
+    fill
+    className="object-cover"
+  />
+  <div className="absolute bottom-[-16px] right-[-16px] w-[60%] h-[60%] border-r-2 border-b-2 border-magenta shadow-[8px_8px_30px_rgba(255,45,120,0.2)]" />
+  <div className="absolute top-[-16px] left-[-16px] w-[40%] h-[40%] border-l-2 border-t-2 border-calipso shadow-[-8px_-8px_30px_rgba(0,229,204,0.2)] z-[1]" />
+</div>
           </SectionReveal>
 
           <SectionReveal delay={200}>
