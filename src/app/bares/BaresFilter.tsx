c309bddef967a3f3692ecfe0e04e13920d85bf29
JPPinkyLabs <jp@pinkylabs.io>
2026-03-18 15:07:00 +0100

    Update BaresFilter.tsx

--- src/app/bares/BaresFilter.tsx
+++ src/app/bares/BaresFilter.tsx
@@ -34,7 +34,7 @@ export default function BaresFilter({ bares, zonas }: BaresFilterProps) {
             className="absolute left-4 top-1/2 -translate-y-1/2 opacity-40"
             width="16"
             height="16"
-            viewBox="0 0 48 48"
+            viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             strokeWidth="2"
